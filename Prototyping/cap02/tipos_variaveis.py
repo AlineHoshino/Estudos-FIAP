61701d55450c07884a64bc9c647796cad1b378fe
ALINE <hoshinoaline@yahoo.com.br>
2023-03-23 22:10:22 -0300

    estudo sobre if, elif, else

---
+++ Prototyping/cap02/tipos_variaveis.py
@@ -0,0 +1,15 @@
+ano = 1989
+nome = "Luke Skywalker"
+saldo = 50.30
+print(("O tipo da variável ano é {}".format(type(ano))))
+print(("O tipo da variável nome é {}".format(type(nome))))
+print(("O tipo da variável saldo é {}".format(type(saldo))))
+
+
+# rm = input("Insira seu RM")
+# idade = input("Insira sua idade")
+# if int(idade) >= 18:
+#     print("Sua participação foi autorizada, aluno de RM {}!".format(rm))
+#     print("Mais informações serão enviadas para seu e-mail cadastrado!")
+# else:
+#     print("Sua participação não foi autorizada por causa da sua idade")
